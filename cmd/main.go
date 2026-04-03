1606640f5da87573a55c6c3abb8af8ee8c6d6967
jupitters <sampaiobale@gmail.com>
2026-04-03 12:12:27 -0300

    starting server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	
+	mux := http.NewServeMux()
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
 }
