4051222e68b18c56f8c0b6240da7f1f86724d5b8
jupitters <sampaiobale@gmail.com>
2026-04-03 12:14:34 -0300

    starting server

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,13 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("GET /movies", listMovies)
+
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func listMovies(w http.ResponseWriter, r *http.Request) http.HandlerFunc {
+
 }
