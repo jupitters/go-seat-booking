6e398f52d93d305dd863bef7fefb62952ba0957f
jupitters <sampaiobale@gmail.com>
2026-04-06 14:59:22 -0300

    handlers

--- internal/booking/handler.go
+++ internal/booking/handler.go
@@ -1,9 +1,15 @@
 package booking
 
+import "net/http"
+
 type handler struct {
 	svc *Service
 }
 
-func NewHandler() *handler {
-	return &handler{}
+func NewHandler(svc *Service) *handler {
+	return &handler{svc}
+}
+
+func (h *handler) ListSeats(w http.ResponseWriter, r *http.Request) {
+	h.svc.store.ListBookings()
 }
